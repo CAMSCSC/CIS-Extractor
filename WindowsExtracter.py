c24d72e964e210798a2de8eb51dc5be7d5a65761
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2022-09-18 15:12:58 -0700

    code block detection

--- WindowsExtracter.py
+++ WindowsExtracter.py
@@ -1,8 +1,12 @@
 import pdfplumber
+from pdfminer.layout import LTChar
+from typing import List, Tuple
 import os 
+import shutil
+import tabulate
 pdf = pdfplumber.open('CIS-windows.pdf')
 
-os.remove("MD-files-windows")
+shutil.rmtree("./MD-files-windows")
 os.mkdir("MD-files-windows")
 
 def getSectionNumber(pgNumber):
@@ -31,16 +35,71 @@ def getRemediation(pgNumber):
             return False # end the function if there is no remediation for that page number section
     # find page number where there is a remediation for the section
 
-    remediationStart = text.find("Remediation:") + 14
+    # Get pdf page as char list (contains font info)
+    chars: List[LTChar] = page.chars
+    # True if font is CourierNewPSMT (code block font)
+    monospaceFontIndices: List[bool] = [chars[i]["fontname"] ==
+                                        "ABCDEE+CourierNewPSMT" for i in range(len(chars))]
+    
+    # Re-generate text using chars
+    text = "".join([char["text"] for char in chars])
+    
+    codeIndices = getTrueRanges(monospaceFontIndices)
+    text = addFormat(text, codeIndices, "`")
+    
+    remediationStart = text.find("Remediation:") + 13
     remediationEnd = text.find("Default Value:")
     if remediationEnd != -1:
         return text[remediationStart : remediationEnd]
     else:
-        return text[remediationStart : len(text) - 14 - len(str(pgNumber))] # 13 is some random number to get rid of the page number could be wrong idk
+        return text[remediationStart : len(text)]
+
+
+def getTrueRanges(list: List[bool]) -> 'List[Tuple[int, int]]':
+    """ Returns the ranges of true values in a boolean list.
+    """
+    indiceRanges: 'List[Tuple[int, int]]' = []
+    
+    alreadyCheckedIndices: int = -1
+    for index, item in enumerate(list):
+        # Skip if already checked
+        if index <= alreadyCheckedIndices:
+            continue
+        # Check for true values after the item if item is true
+        if item == None:
+            continue
+        if item:
+            for index2, item2 in enumerate(list[index::]):
+                index2 += index
+                if item2 == None:
+                    continue
+                # Once the value is not true, return the range up to 1 index lower than it
+                if not item2:
+                    indiceRanges.append((index, index2 - 1))
+                    alreadyCheckedIndices = index2 - 1
+                    break
+            # if the end of the list is reached, return index to end
+            else:
+                # Note: for-else behavior might break in future versions of python
+                indiceRanges.append((index, len(list) - 1))
+                return indiceRanges
+    
+    return indiceRanges
+
+def addFormat(text: str, codeIndices: 'List[Tuple[int, int]]', formatChar: str) -> str:
+    """ Format text as code blocks or bolds by adding ` or * to the start and end
+    """
+    textArr: List[str] = [char for char in text]
+    for index, codeBlock in enumerate(codeIndices):
+         
+        textArr.insert(codeBlock[0] + 2*index, formatChar)
+        textArr.insert(codeBlock[1] + 2*index + 2, formatChar)
+
+    return "".join(textArr)
 
 sectionFilesWritten = []
 
-def writeToFile(sectionNum, remediation):
+def writeToFile(sectionNum: int, remediation: str):
     numOfPeriods = 0
     fileSectionNumber = ""
     folderSectionNumber = ""
@@ -59,7 +118,7 @@ def writeToFile(sectionNum, remediation):
 
     file = open("./MD-files-windows/" + folderSectionNumber + "/" + fileSectionNumber + ".md","a")
     file.write("\n" + "### " + sectionNum + "  ")
-    file.write("\n" + remediation)
+    file.write("\n" + remediation.replace("\u25cf", "").replace("\uf0b7", ""))
     file.close()
 
     file2 = open("./MD-files-windows/" + folderSectionNumber + ".md", "a+")
@@ -68,7 +127,6 @@ def writeToFile(sectionNum, remediation):
         file2.write("\n ```{include} ./" + folderSectionNumber + "/" + fileSectionNumber + ".md" + "\n ``` \n")
         sectionFilesWritten.append(fileSectionNumber)
     file2.close()
-
 
 for i in range(1,20):
     os.mkdir("./MD-files-windows/" + str(i)) # make folders for placing MD files
